166d0a0766342dbe5a17cc3ab58abd1681bd99b2
Johannes Overmann <fgit@joov.de>
2023-03-18 23:07:58 +0100

    Add --sort, --fig-width and --fig-height

--- dataplot.py
+++ dataplot.py
@@ -16,33 +16,36 @@ def main():
     """
     global options
     usage = "Usage: %prog [options] FILES..."
-    version = "0.1.2"
+    version = "0.1.4"
     parser = optparse.OptionParser(usage=usage, version=version)
-    parser.add_option("-o", "--outfile", default="out.png", type="string", help="Output file", metavar="FILE")
-    parser.add_option("-x", "--xcol", default=0, type="int", help="X column. Use -1 for 'index' (if no X column is present in file).", metavar="N")
-    parser.add_option("-y", "--ycol", default=1, type="int", help="Y column", metavar="N")
-    parser.add_option("-c", "--colors", default="rbyg", help="Colors (try rbyg)", metavar="COLSTR")
-    parser.add_option("-s", "--shapes", default="o", help="Dot shapes (try oO.,+x)", metavar="SHAPESTR")
-    parser.add_option("-a", "--addstyle", default="", help="Additional style added to all graphs (use -a - to add lines)", metavar="STYLE")
-    parser.add_option("-f", "--filter", default="", help="Only use lines which match regex RE", metavar="RE")
-    parser.add_option(      "--xlog", default=False, action="store_true", help="Use logscale for X")
-    parser.add_option(      "--xdiv", default=1.0, type="float", help="Divide X values by N (float)", metavar="N")
-    parser.add_option(      "--ymax", default=0, type="float", help="Set Y range to MAX (float)", metavar="MAX")
-    parser.add_option(      "--ymin", default=0, type="float", help="Set Y range to MIN (float)", metavar="MIN")
-    parser.add_option(      "--ylog", default=False, action="store_true", help="Use logscale for Y")
-    parser.add_option(      "--legend", default="upper left", help="Set legend position (default \"upper left\")", metavar="POS")
-    parser.add_option(      "--print-high", default=0, type="float", help="Print lines which Y values higher than N", metavar="N")
-    parser.add_option(      "--print-stats", default=False, action="store_true", help="Print statistics of all Y values")
-    parser.add_option("-v", "--verbose", default=0, action="count", help="Be more verbose")
+    parser.add_option("-o", "--outfile", default="out.png", type="string", help="Output image. Default is 'out.png'. PNG, JPG, PDF and others are supported.", metavar="FILE")
+    parser.add_option("-x", "--xcol", default=-1, type="int", help="X column. Use -1 for 'index' (if no X column is present in file).", metavar="N")
+    parser.add_option("-y", "--ycol", default=1, type="int", help="Y column. Use -v to first to figure out column indices of data.", metavar="N")
+    parser.add_option("-c", "--colors", default="rbyg", help="Set colors. One character per graph. Try rbyg.", metavar="COLSTR")
+    parser.add_option("-s", "--shapes", default="o", help="Set Dot shapes (try oO.,+x).", metavar="SHAPESTR")
+    parser.add_option("-a", "--addstyle", default="", help="Add additional style to all graphs (use -a - to add lines).", metavar="STYLE")
+    parser.add_option("-f", "--filter", default="", help="Only use lines which match regex RE.", metavar="RE")
+    parser.add_option(      "--xlog", default=False, action="store_true", help="Use logscale for X.")
+    parser.add_option(      "--xdiv", default=1.0, type="float", help="Divide X values by N (float).", metavar="N")
+    parser.add_option(      "--ymax", default=0, type="float", help="Set Y range to MAX (float).", metavar="MAX")
+    parser.add_option(      "--ymin", default=0, type="float", help="Set Y range to MIN (float).", metavar="MIN")
+    parser.add_option(      "--ylog", default=False, action="store_true", help="Use logscale for Y.")
+    parser.add_option(      "--sort", default=False, action="store_true", help="Sort Y values. Only makes sense without --xcol.")
+    parser.add_option(      "--legend", default="upper left", help="Set legend position (default \"upper left\").", metavar="POS")
+    parser.add_option(      "--fig-width", default=15, type="float", help="Width of output image in inch at 100 dpi.", metavar="W")
+    parser.add_option(      "--fig-height", default=5, type="float", help="Height of output image in inch at 100 dpi.", metavar="H")
+    parser.add_option(      "--print-high", default=0, type="float", help="Print lines with Y values higher than N.", metavar="N")
+    parser.add_option(      "--print-stats", default=False, action="store_true", help="Print statistics of all Y values.")
+    parser.add_option("-v", "--verbose", default=0, action="count", help="Be more verbose.")
     (options, args) = parser.parse_args()
 
-    # check args
+    # Check args.
     if len(args) < 1:
-        parser.error("please specify at least one file!");
+        parser.error("Please specify at least one file!");
     infile = args[0]
 
-    pyplot.figure(figsize = (15, 5))
-    
+    pyplot.figure(figsize = (options.fig_width, options.fig_height))
+
     # Plot files
     fileindex = 0
     index = 0
@@ -74,6 +77,9 @@ def main():
             index += 1
             total_y += y
 
+        if options.sort:
+            yy = sorted(yy)
+
         color = options.colors[fileindex % len(options.colors)]
         shape = options.shapes[fileindex % len(options.shapes)]
         pyplot.plot(xx, yy, color + shape + options.addstyle, label = infile)
@@ -89,9 +95,10 @@ def main():
 
     if options.print_stats:
         print("Sum={:.1f}, average={:.1f}".format(total_y, total_y / index))
-    
+
     # Save image
-    print("Saving image to '{}'".format(options.outfile))
+    if options.verbose:
+        print("Saving image to '{}'".format(options.outfile))
     pyplot.savefig(options.outfile, bbox_inches='tight', dpi=128)
 
 
