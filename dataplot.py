0864f4a9fd45ce606cc5ff8064fe1ccebc4508d2
Johannes Overmann <fgit@joov.de>
2023-03-19 12:32:37 +0100

    Use argparse instead of optparse

--- dataplot.py
+++ dataplot.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-import optparse
+import argparse
 import matplotlib.pylab as pylab
 import matplotlib.pyplot as pyplot
 import numpy
@@ -15,34 +15,33 @@ def main():
     """Main entry point.
     """
     global options
-    usage = "Usage: %prog [options] FILES..."
-    version = "0.1.4"
-    parser = optparse.OptionParser(usage=usage, version=version)
-    parser.add_option("-o", "--outfile", default="out.png", type="string", help="Output image. Default is 'out.png'. PNG, JPG, PDF and others are supported.", metavar="FILE")
-    parser.add_option("-x", "--xcol", default=-1, type="int", help="X column. Use -1 for 'index' (if no X column is present in file).", metavar="N")
-    parser.add_option("-y", "--ycol", default=1, type="int", help="Y column. Use -v to first to figure out column indices of data.", metavar="N")
-    parser.add_option("-c", "--colors", default="rbyg", help="Set colors. One character per graph. Try rbyg.", metavar="COLSTR")
-    parser.add_option("-s", "--shapes", default="o", help="Set Dot shapes (try oO.,+x).", metavar="SHAPESTR")
-    parser.add_option("-a", "--addstyle", default="", help="Add additional style to all graphs (use -a - to add lines).", metavar="STYLE")
-    parser.add_option("-f", "--filter", default="", help="Only use lines which match regex RE.", metavar="RE")
-    parser.add_option(      "--xlog", default=False, action="store_true", help="Use logscale for X.")
-    parser.add_option(      "--xdiv", default=1.0, type="float", help="Divide X values by N (float).", metavar="N")
-    parser.add_option(      "--ymax", default=0, type="float", help="Set Y range to MAX (float).", metavar="MAX")
-    parser.add_option(      "--ymin", default=0, type="float", help="Set Y range to MIN (float).", metavar="MIN")
-    parser.add_option(      "--ylog", default=False, action="store_true", help="Use logscale for Y.")
-    parser.add_option(      "--sort", default=False, action="store_true", help="Sort Y values. Only makes sense without --xcol.")
-    parser.add_option(      "--legend", default="upper left", help="Set legend position (default \"upper left\").", metavar="POS")
-    parser.add_option(      "--fig-width", default=15, type="float", help="Width of output image in inch at 100 dpi.", metavar="W")
-    parser.add_option(      "--fig-height", default=5, type="float", help="Height of output image in inch at 100 dpi.", metavar="H")
-    parser.add_option(      "--print-high", default=0, type="float", help="Print lines with Y values higher than N.", metavar="N")
-    parser.add_option(      "--print-stats", default=False, action="store_true", help="Print statistics of all Y values.")
-    parser.add_option("-v", "--verbose", default=0, action="count", help="Be more verbose.")
-    (options, args) = parser.parse_args()
+    usage = "%(prog)s [options] FILES..."
+    version = "0.1.5"
+    parser = argparse.ArgumentParser(usage = usage + "\n(Version " + version + ")")
+    parser.add_argument("files", nargs="*", help="Files to process.")
+    parser.add_argument("-o", "--outfile", default="out.png", help="Output image. Default is 'out.png'. PNG, JPG, PDF and others are supported.", metavar="FILE")
+    parser.add_argument("-x", "--xcol", default=-1, type=int, help="X column. Use -1 for 'index' (if no X column is present in file).", metavar="N")
+    parser.add_argument("-y", "--ycol", default=1, type=int, help="Y column. Use -v to first to figure out column indices of data.", metavar="N")
+    parser.add_argument("-c", "--colors", default="rbyg", help="Set colors. One character per graph. Try rbyg.", metavar="COLSTR")
+    parser.add_argument("-s", "--shapes", default="o", help="Set Dot shapes (try oO.,+x).", metavar="SHAPESTR")
+    parser.add_argument("-a", "--addstyle", default="", help="Add additional style to all graphs (use -a - to add lines).", metavar="STYLE")
+    parser.add_argument("-f", "--filter", default="", help="Only use lines which match regex RE.", metavar="RE")
+    parser.add_argument(      "--xlog", default=False, action="store_true", help="Use logscale for X.")
+    parser.add_argument(      "--xdiv", default=1.0, type=float, help="Divide X values by N (float).", metavar="N")
+    parser.add_argument(      "--ymax", default=0, type=float, help="Set Y range to MAX (float).", metavar="MAX")
+    parser.add_argument(      "--ymin", default=0, type=float, help="Set Y range to MIN (float).", metavar="MIN")
+    parser.add_argument(      "--ylog", default=False, action="store_true", help="Use logscale for Y.")
+    parser.add_argument(      "--sort", default=False, action="store_true", help="Sort Y values. Only makes sense without --xcol.")
+    parser.add_argument(      "--legend", default="upper left", help="Set legend position (default \"upper left\").", metavar="POS")
+    parser.add_argument(      "--fig-width", default=15, type=float, help="Width of output image in inch at 100 dpi.", metavar="W")
+    parser.add_argument(      "--fig-height", default=5, type=float, help="Height of output image in inch at 100 dpi.", metavar="H")
+    parser.add_argument(      "--print-high", default=0, type=float, help="Print lines with Y values higher than N.", metavar="N")
+    parser.add_argument(      "--print-stats", default=False, action="store_true", help="Print statistics of all Y values.")
+    parser.add_argument("-v", "--verbose", default=0, action="count", help="Be more verbose.")
+    options = parser.parse_args()
 
-    # Check args.
-    if len(args) < 1:
-        parser.error("Please specify at least one file!");
-    infile = args[0]
+    if len(options.files) == 0:
+        parser.error("Please specify at least one file!")
 
     pyplot.figure(figsize = (options.fig_width, options.fig_height))
 
@@ -50,7 +49,7 @@ def main():
     fileindex = 0
     index = 0
     total_y = 0
-    for infile in args:
+    for infile in options.files:
         with open(infile) as file:
             lines = file.readlines()
         if options.filter:
